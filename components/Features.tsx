dbf3868e01832328e6789f7b8885e2af1ae289ec
Guillermo Eyherabide <eyherabideg@gmail.com>
2023-11-30 11:09:46 -0300

    fix

--- components/Features.tsx
+++ components/Features.tsx
@@ -11,11 +11,11 @@ export default function Features() {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto">
-        <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+        <div className="flex flex-wrap w-full mb-8 flex-col items-center text-center">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-900 mb-4">
             How it works
           </h1>
-          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500 mb-8">
             To complete a challenge, you'll need to follow some steps. We'll be
             there to help you along the way.
           </p>
@@ -29,7 +29,7 @@ export default function Features() {
         </div>
         <div className="flex flex-wrap -m-4">
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <FontAwesomeIcon className="w-6 h-6" icon={faMagnifyingGlass} />
               </div>
@@ -43,7 +43,7 @@ export default function Features() {
             </div>
           </div>
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <svg
                   fill="none"
@@ -69,7 +69,7 @@ export default function Features() {
             </div>
           </div>
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <svg
                   fill="none"
@@ -93,7 +93,7 @@ export default function Features() {
             </div>
           </div>
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <svg
                   fill="none"
@@ -118,7 +118,7 @@ export default function Features() {
             </div>
           </div>
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <svg
                   fill="none"
@@ -143,7 +143,7 @@ export default function Features() {
           </div>
 
           <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-200 p-6 rounded-lg">
+            <div className="border border-gray-200 p-6 rounded-lg h-full">
               <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-4">
                 <svg
                   fill="none"
